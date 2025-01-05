ff4ed7078095373d85723dc89683e9996c8c80e4
awsajju <aws.devops.123aws@gmail.com>
2025-01-06 00:05:52 +0530

    backend changes

--- backend.sh
+++ backend.sh
@@ -64,7 +64,7 @@ validate $? "unzip backend"
 npm install &>>$LOG_FILE_NAME
 validate $? "installing dependencies"
 
-cp /home/ec2-user/EXPENSESPROJECT_AUTOMATION/backend.services /etc/systemd/system/backend.service
+cp /c/Users/user/EXPENSESPROJECT_AUTOMATION/backend.services /etc/systemd/system/backend.service
 
 # prepare MYSQL Schema
 
