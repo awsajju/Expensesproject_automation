e61417bc7d842409d2785752ec0ce78f35934815
awsajju <aws.devops.123aws@gmail.com>
2025-01-06 01:03:38 +0530

    backend changes

--- frontend.sh
+++ frontend.sh
@@ -46,7 +46,7 @@ validate $? "starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
 
-validate $? "removing the existing code" 
+validate $? "removing the existing code"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
 
