322181cbbc41bcdbeb4c3505ae34ce968078febf
awsajju <aws.devops.123aws@gmail.com>
2025-01-06 00:04:25 +0530

    backend changes

--- backend.sh
+++ backend.sh
@@ -56,6 +56,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 validate $? "Downloading backend"
 
 cd /app
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 validate $? "unzip backend"
@@ -79,5 +80,5 @@ validate $? "Daemon reloading"
 systemctl enable backend &>>$LOG_FILE_NAME
 validate $? "Enabling backend"
 
-systemctl start backend &>>$LOG_FILE_NAME
+systemctl restart backend &>>$LOG_FILE_NAME
 validate $? "start backend"
