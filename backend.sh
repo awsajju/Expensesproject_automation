bf5cb2baa9d6b8c7117db9016892ff33f9b092b4
awsajju <aws.devops.123aws@gmail.com>
2025-01-05 23:48:41 +0530

    backend changes

--- backend.sh
+++ backend.sh
@@ -13,10 +13,10 @@ LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP"
 
 validate(){
     if [ $1 -ne 0 ];then
-        echo "$2 $R fialure $N"
+        echo -e "$2 $R fialure $N"
         exit 1
     else
-        echo "$2 $G Success $N"
+        echo -e "$2 $G Success $N"
     fi
 }
 
